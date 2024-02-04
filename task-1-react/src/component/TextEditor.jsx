189bdd7a38a4984d3d93e48868c756df26f3ea3c
karthik <karthikmudhigonda1@gmail.com>
2024-02-04 19:50:26 +0530

    update task-1 and task-3

--- task-1-react/src/component/TextEditor.jsx
+++ task-1-react/src/component/TextEditor.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import { useDropzone } from "react-dropzone";
 import { saveAs } from "file-saver";
+import { format } from "date-fns";
 import "./TextEditor.css";
 
 const TextEditor = () => {
+  // State to manage the text content and file name
   const [text, setText] = useState("");
   const [fileName, setFileName] = useState("");
 
   const onDrop = (acceptedFiles) => {
+    //FileReader to read the content of the dropped file
     const fileReader = new FileReader();
     fileReader.onload = (event) => {
       const content = event.target.result;
@@ -18,19 +21,19 @@ const TextEditor = () => {
   };
 
   const onSaveClick = () => {
-    saveTextToFile(text, `text_file_${Date.now}.txt`);
+    // Use the file-saver library to trigger a file save dialog
+    saveAs(
+      new Blob([text], { type: "text/plain;charset=utf-8" }),
+      generateFileName()
+    );
+    setText("");
+    setFileName("");
   };
 
-  const saveTextToFile = (content) => {
-    const currentDate = new Date();
-    const formattedDate = currentDate
-      .toISOString()
-      .replace(/[-:T.]/g, "")
-      .slice(2, 14); // Format date as "YYYYMMDDHHmmss"
-    const fileName = `text_file_${formattedDate}.txt`;
-
-    const blob = new Blob([content], { type: "text/plain;charset=utf-8" });
-    saveAs(blob, fileName);
+  const generateFileName = () => {
+    // Format the date using date-fns
+    const formattedDate = format(new Date(), "yyMMddHHmmss");
+    return `text_file_${formattedDate}.txt`;
   };
 
   const { getRootProps, getInputProps } = useDropzone({
